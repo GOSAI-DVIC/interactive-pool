d54fe3834982e62f5d0952969bf08336f3618c99
Maxime <mbroussart@gmail.com>
2022-03-15 11:46:37 +0100

    Yawa initial commit

--- apps/yawa/processing.py
+++ apps/yawa/processing.py
@@ -1,23 +1,28 @@
 from core.application import BaseApplication
-import time
+import socketio
 
 class Application(BaseApplication):
-    """menu"""
+    """live"""
 
     def __init__(self, name, hal, server, manager):
         super().__init__(name,hal,server,manager)
         self.requires["ball"] = ["ball_data"]
-        self.time = 0
 
+        # self.sio = socketio.Client()
+
+        # self.sio.connect("https://vps.thomasjuldo.com/", socketio_path="/realtimepool/socket.io")
+        
 
     def listener(self, source, event, data):
         super().listener(source, event, data)
 
         if source == "ball" and event == "ball_data" and data is not None:
-            # print((time.time()-self.time)*1000)
-            self.time = time.time()
-            self.server.send_data("ball", data)
+            self.data = data
 
-        # if source == "cue" and event == "cue_data":
-        #     self.data = self.hal.get_driver_event_data("cue", "cue_data")
-        #     self.server.send_data("cue", self.data)
+            l= []
+            for x,y in self.data:
+                x/=1888
+                y/=1049
+                l.append([x,y])
+
+            self.sio.emit("update_data",l)
