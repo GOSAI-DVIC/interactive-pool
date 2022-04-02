5fee9aa08707aefb338ffdf4cf5d6caea58f6db1
Maxime <mbroussart@gmail.com>
2022-04-02 11:04:49 +0200

    Initializing simple Yawa App

--- apps/yawa/display.js
+++ apps/yawa/display.js
@@ -1,13 +1,26 @@
-export const live = new p5(( sketch ) => {
+export const yawa = new p5(( sketch ) => {
     sketch.name = "live"
     sketch.activated = false
 
-
+    let input, button, greeting;
+    
     sketch.set = (width, height, socket) => {
+        
 
+        sketch.selfCanvas = sketch.createCanvas(width, height).position(0, 0);
+        input = sketch.createInput()
+        input.position(20, 65);
 
-        //sketch.selfCanvas = sketch.createCanvas(width, height).position(0, 0);
+        button = sketch.createButton('submit');
+        button.position(input.x + input.width, 65);
+        button.mousePressed(greet);
 
+        greeting = createElement('h2', 'what is your name?');
+        greeting.position(20, 5);
+        greeting.style("background-color", 'white');
+
+        textAlign(CENTER);
+        textSize(50);
         socket.on("ball", (data) => {
         });
 
@@ -19,7 +32,7 @@ export const live = new p5(( sketch ) => {
     }
 
     sketch.windowResized = () => {
-        //resizeCanvas(windowWidth, windowHeight);
+        // resizeCanvas(windowWidth, windowHeight);
     }
 
     sketch.pause = () => {
@@ -32,5 +45,23 @@ export const live = new p5(( sketch ) => {
     }
 
     sketch.show = () => {
+
     }
+
+    function greet() {
+        const name = input.value();
+        greeting.html('hello ' + name + '!');
+        input.value('');
+      
+        for (let i = 0; i < 200; i++) {
+          push();
+          fill(random(255), 255, 255);
+          translate(random(width), random(height));
+          rotate(random(2 * PI));
+          text(name, 0, 0);
+          pop();
+        }
+
+        socket.emit("sound", true);
+      }
 });
--- apps/yawa/working/socket_UDP_client.py
+++
@@ -1,5 +0,0 @@
-import socket
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-adress= ("172.21.72.160",5000)
-response = "Salut"
-sock.sendto(response.encode(),adress)
