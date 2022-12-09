3e397202a0c842592c8f2bb5c822af10d9c46996
cyprienmarque <cyprien.marque@edu.devinci.fr>
2022-12-09 14:12:45 -0100

    Playing music when ball stopped (fixed)

--- apps/music/components/balls.js
+++ apps/music/components/balls.js
@@ -9,6 +9,10 @@ export class Balls {
         this.balls = []
         this.ball_nb = 0
         this.previous_ball_nb = max_ball;
+        this.ballmoving = []
+        this.XBallFrameBefore = []
+        this.YBallFrameBefore = []
+        this.frame = 0;
 
         for(let i=0; i<max_ball; i++)
         {
@@ -18,8 +22,10 @@ export class Balls {
     }
 
     show(sketch, pianoA4, pianoC4) {
-        this.impactDetection(sketch, pianoA4, pianoC4)
-        this.impactmur(sketch, pianoA4, pianoC4)
+        this.isstopped()
+        this.impactDetection(sketch, pianoA4)
+        this.impactmur(sketch, pianoC4)
+        
         sketch.push();
         for (let ball of this.balls)
         {
@@ -57,51 +63,72 @@ export class Balls {
         this.previous_ball_nb = this.ball_nb
     }
 
-    impactDetection(sketch, pianoA4, pianoC4){
+    isstopped(){
+        if(this.frame > 0){
+            for(let a = 0; a < this.ball_nb; a++){
+                if(this.balls[a].x == this.XBallFrameBefore[a] && this.balls[a].y == this.YBallFrameBefore[a]){
+                    this.ballmoving[a] = 'false';
+                }
+                else{
+                    this.ballmoving[a] = 'true';
+                }
+            }
+            for(let a = 0; a < this.ball_nb; a++){
+                this.XBallFrameBefore[a] = this.balls[a].x
+                this.YBallFrameBefore[a] = this.balls[a].y
+            }
+        }
+        this.frame++
+    }
+
+
+    impactDetection(sketch, pianoA4){
         for(let i = 0; i < this.ball_nb; i++){
             for(let j = 0; j < this.ball_nb; j++){
                 if(i!=j){
                     if(80>sqrt(sq(this.balls[i].x-this.balls[j].x)+sq(this.balls[i].y-this.balls[j].y))){
-                        
                         sketch.fill(200,0,0)
                         sketch.circle(260,300,100)
                         sketch.circle(this.balls[i].x,this.balls[i].y, 30)
                         pianoA4.play()
-             
                     }
                 }
             }
         }
     }
 
-    impactmur(sketch, pianoA4, pianoC4){
-        this.impactwallwidth(sketch, pianoA4, pianoC4)
-        this.impactwallheight(sketch, pianoA4, pianoC4)
+    impactmur(sketch, pianoC4){
+        this.impactwallwidth(sketch, pianoC4)
+        this.impactwallheight(sketch, pianoC4)
     }
 
-    impactwallwidth(sketch, pianoA4, pianoC4){
+    impactwallwidth(sketch, pianoC4){
         for(let i = 0; i < this.ball_nb; i++){
-            if(this.balls[i].x > width || this.balls[i].x < 0){
-  
-                sketch.fill(0,200,0)
-                sketch.circle(260,300,100)
-                sketch.circle(this.balls[i].x,this.balls[i].y, 60)
-                pianoC4.play()
+            if(this.ballmoving[i] == 'true'){
+                if(this.balls[i].x > 1830 || this.balls[i].x < 90){
+                    sketch.fill(0,200,0)
+                    sketch.circle(260,300,100)
+                    sketch.circle(this.balls[i].x,this.balls[i].y, 60)
+                    pianoC4.play()
+                }
             }
         }
     }
 
-    impactwallheight(sketch, pianoA4, pianoC4){
+    impactwallheight(sketch, pianoC4){
         for(let j = 0; j < this.ball_nb; j++){
-            if(this.balls[j].y > height || this.balls[j].y < 0){
-  
-                sketch.fill(0,200,0)
-                sketch.circle(260,300,100)
-                sketch.circle(this.balls[j].x,this.balls[j].y, 30)
-                pianoC4.play()
+            if(this.ballmoving[j] == 'true'){
+                if(this.balls[j].y > 1000 || this.balls[j].y < 75){
+                    sketch.fill(0,200,0)
+                    sketch.circle(260,300,100)
+                    sketch.circle(this.balls[j].x,this.balls[j].y, 30)
+                    pianoC4.play()
+                }
             }
         }
     }
+
+
 
 
 }
